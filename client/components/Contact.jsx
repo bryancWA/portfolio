9cfec4ecc0b13e71bdf098fc7f4dce7af2bb58c8
Bryan Clark <clark.bryanr@gmail.com>
2021-06-19 19:21:34 -0700

    additional styling...almost there

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -7,16 +7,20 @@ const Contact = () => {
   return (
     <div id="contact-div" >
       <h2>Contact Info</h2>
-      <img src={image} style={{height: '200px', width: '175px', borderRadius: '10px'}}
-      alt="Bryan Clark profile picture"/>
-      <div id="github">
-        <p><img src={image2} alt="github icon" style={{height: '32px', width: '32px'}}/>@bryancWA</p>
-      </div>
-      <div id="email-div">
-       <p><i className="far fa-envelope"></i>clark.bryanr@gmail.com</p>
-      </div>
-      <div id="linkedin-div">
-        <p><i className="fab fa-linkedin"></i>https://www.linkedin.com/in/bryan-r-clark/</p>
+      <div id="contact-components">
+        <img src={image} style={{height: '200px', width: '175px', borderRadius: '50%'}}
+        alt="Bryan Clark profile picture"/>
+        <div id="contact-info-div">
+          <div className="contact-info">
+            <p><img src={image2} alt="github icon" style={{height: '20px', width: '20px'}}/>bryancWA</p>
+          </div>
+          <div className="contact-info">
+          <p><i className="far fa-envelope"></i>clark.bryanr@gmail.com</p>
+          </div>
+          <div className="contact-info">
+            <p><i className="fab fa-linkedin"></i>https://www.linkedin.com/in/bryan-r-clark/</p>
+          </div>
+        </div>
       </div>
     </div>
   )
